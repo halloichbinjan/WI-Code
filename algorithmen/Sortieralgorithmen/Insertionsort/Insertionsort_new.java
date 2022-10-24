fa002533a65b3c36a339e13b08e825785567f9d1
Jan Schmidt <jan.schmidt@wetteronline.de>
2022-10-24 11:11:55 +0200

    .

--- algorithmen/Sortieralgorithmen/Insertionsort/Insertionsort_new.java
+++ algorithmen/Sortieralgorithmen/Insertionsort/Insertionsort_new.java
@@ -74,7 +74,7 @@ public class Insertionsort_new {
 
     static void printArray() {
         int n = arr.length;
-        for (int i = 0; i < n; ++i)
+        for (int i = 0; i < n; i++)
             System.out.print(arr[i] + " ");
     }
 }
