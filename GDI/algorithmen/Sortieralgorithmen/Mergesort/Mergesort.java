bdc209e06529c490b4bbf8da72b2ea2cca75e758
halloichbinjan <janfriedrichschmidt@gmail.com>
2022-11-14 12:44:13 +0100

    aufgabe 4 - 1

--- GDI/algorithmen/Sortieralgorithmen/Mergesort/Mergesort.java
+++ GDI/algorithmen/Sortieralgorithmen/Mergesort/Mergesort.java
@@ -1,20 +1,45 @@
 public class Mergesort {
-    static int[] arr = { 3, 2, 4, 5, 6, 7, 3, 2, 34, 5 };
+    static int[] arr = { 3, 2, 4, 5, 6, 7, 3, 1, 34, 5 };
 
     public static void main(String[] args) {
-        mergesort(arr);
-    }
-
-    static void mergesort(int[] pArr) {
-        msHelper(pArr, 0, pArr.length - 1);
-    }
-
-    static void msHelper(int[] pArr, int from, int to) {
-        if (to - from <= 1) {
-            return;
+        mergeSort(arr, 0, arr.length - 1);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
         }
-        int middle = (from + to) / 2;
-        msHelper(pArr, from, middle);
-        msHelper(pArr, middle + 1, to);
     }
+
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+
+    public static void merge(int[] arr, int left, int mid, int right) {
+        int[] temp = new int[arr.length];
+        for (int i = left; i <= right; i++) {
+            temp[i] = arr[i];
+        }
+        int i = left;
+        int j = mid + 1;
+        int k = left;
+        while (i <= mid && j <= right) {
+            if (temp[i] <= temp[j]) {
+                arr[k] = temp[i];
+                i++;
+            } else {
+                arr[k] = temp[j];
+                j++;
+            }
+            k++;
+        }
+        while (i <= mid) {
+            arr[k] = temp[i];
+            k++;
+            i++;
+        }
+    }
+
 }
---
+++ Programmierung 1/notenverwaltung.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Aufgabe 4 - 1
+void scanNoten(int *noten, int length);
+void printNoten(int noten[], int length);
+void statistic(int noten[], int length);
+void changeIndex(int *noten, int length);
+
+int main(void)
+{
+    int noten[5];
+    int length = sizeof(noten) / sizeof(noten[0]);
+
+    scanNoten(noten, length);
+    changeIndex(noten, length);
+
+    return EXIT_SUCCESS;
+}
+
+void scanNoten(int *noten, int length)
+{
+    for (int i = 0; i < length; i++)
+    {
+        printf("Bitte geben Sie die Note %d ein: ", i + 1);
+        int note;
+        scanf("%d", &note);
+        if (note < 0 || note > 5)
+        {
+            printf("Fehler: Die Note muss zwischen 0 und 5 liegen!\n");
+            i--;
+        }
+        else
+        {
+            noten[i] = note;
+        }
+    }
+}
+
+void printNoten(int noten[], int length)
+{
+    for (int i = 0; i < length; i++)
+    {
+        printf("Note %d: %d\n", i + 1, noten[i]);
+    }
+}
+
+void statistic(int noten[], int length)
+{
+
+    int zeros, ones, twos, threes, fours, fives;
+    zeros = ones = twos = threes = fours = fives = 0;
+    for (int i = 0; i < length; i++)
+    {
+        switch (noten[i])
+        {
+
+        case 0:
+            zeros++;
+            break;
+        case 1:
+            ones++;
+            break;
+        case 2:
+            twos++;
+            break;
+        case 3:
+            threes++;
+            break;
+        case 4:
+            fours++;
+            break;
+        case 5:
+            fives++;
+            break;
+        }
+    }
+
+    float average = (ones * 1 + twos * 2 + threes * 3 + fours * 4 + fives * 5) / (float)(length - zeros);
+
+    printf("\nNicht mitgeschrieben haben: %d\n\n", zeros);
+    printf("Anzahl der 1er: %d\n", ones);
+    printf("Anzahl der 2er: %d\n", twos);
+    printf("Anzahl der 3er: %d\n", threes);
+    printf("Anzahl der 4er: %d\n", fours);
+    printf("Anzahl der 5er: %d\n\n", fives);
+    printf("Durchschnitt: %.2f\n", average);
+}
+
+void changeIndex(int *noten, int length)
+{
+    int index;
+    int value;
+    printf("Welcher Index soll geändert werden? ");
+    scanf("%d", &index);
+    // Index muss zwischen 0 und längen des Arrays liegen
+    if (index < 0 || index > length)
+    {
+        printf("Fehler: Der Index muss zwischen 0 und %d liegen!\n", length);
+    }
+    else
+    {
+        printf("Welchen Wert soll der Index haben? ");
+        scanf("%d", &value);
+        if (value < 0 || value > 5)
+        {
+            printf("Fehler: Die Note muss zwischen 0 und 5 liegen!\n");
+        }
+        else
+        {
+            noten[index] = value;
+        }
+    }
+}
